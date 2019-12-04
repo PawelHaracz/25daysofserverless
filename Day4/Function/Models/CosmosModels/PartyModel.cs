84a84214afc1c05067de3c9a10cececd30ed6a13
Pawel Haracz <haracz5@live.com>
2019-12-04 17:29:55 +0100

    Day 4

---
+++ Day4/Function/Models/CosmosModels/PartyModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Day4.Models.CosmosModels
+{
+    public class PartyModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+        
+        [JsonProperty(PropertyName = "organizer")]
+        public string Organizer { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public IList<Food> FoodList { get; set; }
+    }
+
+    public class Food
+    {
+        public  string Name { get; set; }
+        public string Owner { get; set; }
+    }
+}
